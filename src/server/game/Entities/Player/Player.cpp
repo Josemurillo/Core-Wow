72350ce7b55b7dde04e6dcaa84af254b7921891a
Josemurillo <jmurillo9999@hotmail.com>
2012-03-20 02:53:13 +0100

    Reparacion del error de meter mas jugadores de la cuenta en una instance.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -18196,6 +18196,13 @@ bool Player::CheckInstanceLoginValid()
             return false;
     }
 
+	if (GetMap()->GetPlayersCountExceptGMs() > ((InstanceMap*)GetMap())->GetMaxPlayers())
+    {
+        SendTransferAborted(GetMap()->GetId(), TRANSFER_ABORT_MAX_PLAYERS);
+		TeleportTo(m_homebindMapId, m_homebindX, m_homebindY, m_homebindZ, GetOrientation());
+        return false;
+    }
+
     // do checks for satisfy accessreqs, instance full, encounter in progress (raid), perm bind group != perm bind player
     return sMapMgr->CanPlayerEnter(GetMap()->GetId(), this, true);
 }
